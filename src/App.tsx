b45283be5a5b95955b3205603de463e6848c4a61
ibernabel <id.bernabel@gmail.com>
2023-08-23 12:52:50 -0400

    feat: Complete App Firt Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-//import { useState } from 'react'
+// import { Copyright } from './components/Copyright'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Todos } from './components/Todos'
+import { useTodos } from './hooks/useTodos'
 
-const App = (): JSX.Element => {
-	return (
-		<h1>Todo MVC</h1>
-	)
+const App: React.FC = () => {
+  const {
+    activeCount,
+    completedCount,
+    filterSelected,
+    handleClearCompleted,
+    handleCompleted,
+    handleFilterChange,
+    handleRemove,
+    handleSave,
+    handleUpdateTitle,
+    todos: filteredTodos
+  } = useTodos()
 
+  return (
+    <>
+      <div className='todoapp'>
+        <Header saveTodo={handleSave} />
+        <Todos
+          removeTodo={handleRemove}
+          setCompleted={handleCompleted}
+          setTitle={handleUpdateTitle}
+          todos={filteredTodos}
+        />
+        <Footer
+          handleFilterChange={handleFilterChange}
+          completedCount={completedCount}
+          activeCount={activeCount}
+          filterSelected={filterSelected}
+          onClearCompleted={handleClearCompleted}
+        />
+      </div>
+      {/* <Copyright /> */}
+    </>
+  )
 }
 
 export default App
