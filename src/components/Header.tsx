4de7e2ae6876f8118bb568ff37493166d16cf68a
ibernabel <id.bernabel@gmail.com>
2023-08-30 22:45:01 -0400

    fix: Fix todoLoading layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC<Props> = ({ saveTodo }) => {
       <h1>todo
         <img
           style={{ width: '60px', height: 'auto' }}
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'></img>
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png' alt='Title'></img>
       </h1>
 
       <CreateTodo saveTodo={saveTodo} />
--- src/components/TodosLoading.tsx
+++ src/components/TodosLoading.tsx
@@ -1,57 +1,57 @@
 export const TodosLoading: React.FC = () => {
   return (
 
-     <div role="status" class="max-w-xl animate-pulse space-y-6 divide-y divide-gray-50 p-4 dark:divide-gray-700 md:p-6">
-  <div class="flex items-center justify-between">
-    <div class="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-    <div class="flex w-full max-w-[440px] items-center space-x-2">
-      <div class="h-2.5 w-2/12 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-      <div class="h-2.5 w-1/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+     <div role="status" className="max-w-xl animate-pulse space-y-6 divide-y divide-gray-50 py-4 px-1.5 dark:divide-gray-700 md:p-6">
+  <div className="flex items-center justify-between">
+    <div className="flex h-7 w-8 mr-5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+    <div className="flex w-full max-w-md items-center space-x-2">
+      <div className="h-3 w-2/12 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div className="h-3 w-1/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
     </div>
-    <div class="h-8 w-8"></div>
+    <div className="flex h-8 w-8"></div>
   </div>
-  <div class="flex items-center justify-between pt-4">
-    <div class="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-    <div class="flex w-full max-w-[440px] items-center space-x-2">
-      <div class="h-2.5 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-      <div class="h-2.5 w-1/4 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+  <div className="flex items-center justify-between pt-4">
+    <div className="flex h-7 w-8 mr-5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+    <div className="flex w-full max-w-md items-center space-x-2">
+      <div className="h-3 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div className="h-3 w-1/4 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
     </div>
-    <div class="h-8 w-8"></div>
+    <div className="flex h-8 w-8"></div>
   </div>
-  <div class="flex items-center justify-between pt-4">
-    <div class="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-    <div class="flex w-full max-w-[400px] items-center space-x-2">
-      <div class="h-2.5 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-      <div class="h-2.5 w-1/2 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+  <div className="flex items-center justify-between pt-4">
+    <div className="flex h-7 w-8 mr-5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+    <div className="flex w-full max-w-md items-center space-x-2">
+      <div className="h-3 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div className="h-3 w-1/2 rounded-full bg-gray-300 dark:bg-gray-600"></div>
     </div>
-    <div class="h-8 w-8"></div>
+    <div className="flex h-8 w-8"></div>
   </div>
-  <div class="flex items-center justify-between pt-4">
-    <div class="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-    <div class="flex w-full max-w-[440px] items-center space-x-2">
-      <div class="h-2.5 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-      <div class="h-2.5 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+  <div className="flex items-center justify-between pt-4">
+    <div className="flex h-7 w-8 mr-5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+    <div className="flex w-full max-w-md items-center space-x-2">
+      <div className="h-3 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div className="h-3 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
     </div>
-    <div class="h-8 w-8"></div>
+    <div className="flex h-8 w-8"></div>
 
   </div>
-  <div class="flex items-center justify-between pt-4">
-    <div class="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-    <div class="flex w-full max-w-[440px] items-center space-x-2">
-      <div class="h-2.5 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-      <div class="h-2.5 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+  <div className="flex items-center justify-between pt-4">
+    <div className="flex h-7 w-8 mr-5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+    <div className="flex w-full max-w-md items-center space-x-2">
+      <div className="h-3 w-2/12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/3 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+      <div className="h-3 w-1/2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
     </div>
-    <div class="h-8 w-8"></div>
+    <div className="flex h-8 w-8"></div>
 
   </div>
-  <span class="sr-only">Loading...</span>
+  <span className="sr-only">Loading...</span>
 </div>
 
   )
